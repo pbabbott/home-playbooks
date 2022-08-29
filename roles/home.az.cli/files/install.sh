8ec787846ed6abb4fef4e35a72164379622b270d
Brandon Abbott <pabbott@starbucks.com>
2022-08-29 13:39:00 -0700

    Add home.az.cli role

---
+++ roles/home.az.cli/files/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -sL https://packages.microsoft.com/keys/microsoft.asc \
+    | gpg --dearmor \
+    | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg \
+    > /dev/null
+
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=arm64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
