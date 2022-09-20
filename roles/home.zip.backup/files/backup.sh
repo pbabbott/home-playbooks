fadd3793a51b917555f753ef527984b33cad6339
Brandon Abbott <pabbott@starbucks.com>
2022-09-20 12:33:42 -0700

    Move temporary code out. Fix mealie backup cron

--- roles/home.zip.backup/files/backup.sh
+++ roles/home.zip.backup/files/backup.sh
@@ -3,10 +3,21 @@
 # BEGIN ANSIBLE MANAGED BLOCK
 # END ANSIBLE MANAGED BLOCK
 
+dt=$(date)
+echo "Current time is: $dt"
 
 tar -czvf $dest $path
 
+echo "✔️ Tar complete."
+# export HOME=/home/pi
+# export PYTHONPATH=/usr/local/lib/python3.9/dist-packages
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+echo $PATH | tr ":" "\n"
+
 az login --service-principal -u $username -p $password --tenant $tenant
+
+echo "✔️ Login complete."
 
 name=$(basename $dest)
 
@@ -17,3 +28,5 @@ az storage blob upload \
     --file $dest \
     --overwrite \
     --auth-mode login
+
+echo "✔️ Upload complete."
