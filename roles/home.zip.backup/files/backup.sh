04152273fa8c01e31f4e48b39e0056055c526299
Brandon Abbott <pabbott@gmail.com>
2023-01-19 20:37:08 -0800

    Make zip backup easier to use, Add grafana to rpi

--- roles/home.zip.backup/files/backup.sh
+++ roles/home.zip.backup/files/backup.sh
@@ -9,11 +9,7 @@ echo "Current time is: $dt"
 tar -czvf $dest $path
 
 echo "✔️ Tar complete."
-# export HOME=/home/pi
-# export PYTHONPATH=/usr/local/lib/python3.9/dist-packages
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
-echo $PATH | tr ":" "\n"
 
 az login --service-principal -u $username -p $password --tenant $tenant
 
@@ -30,3 +26,7 @@ az storage blob upload \
     --auth-mode login
 
 echo "✔️ Upload complete."
+
+rm $dest
+
+echo "✔️ Deleted file $dest from host."
