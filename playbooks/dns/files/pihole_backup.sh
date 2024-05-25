93f7ff18c2cfa9153663249928785a4df54e8f86
Brandon Abbott <pbabbott@gmail.com>
2024-05-25 13:25:08 -0500

    Attempt to fix broken pihoile cron backups

--- playbooks/dns/files/pihole_backup.sh
+++ playbooks/dns/files/pihole_backup.sh
@@ -5,3 +5,5 @@ CONTAINER_ID=$(docker ps -aqf "name=pihole")
 
 # Execute the commands inside the container
 docker exec -it "$CONTAINER_ID" /bin/bash -c "cd /backups && pihole -a -t"
+
+echo "Job complete at $(date)"
--- playbooks/dns/files/remove_old_backups.sh
+++ playbooks/dns/files/remove_old_backups.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Find and remove old backup files older than 30 days
-find /backups -maxdepth 1 -name "pi-hole-*" -type f -mtime +30 -delete
+find /mnt/pihole_backups -maxdepth 1 -name "pi-hole-*" -type f -mtime +30 -delete
+
+echo "Job complete at $(date)"
